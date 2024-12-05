c76132ae67fcccb323f95b88dc99bfef48d50059
pukkok <krystalgggg@gmail.com>
2024-12-05 17:44:21 +0900

    사용법 파악 주석 처리

--- src/sql-lite/sqlite-database.js
+++ src/sql-lite/sqlite-database.js
@@ -1,24 +1,27 @@
 import {DatabaseSync} from 'node:sqlite'
 const db = new DatabaseSync(':memory:')
 
-console.log(db)
-
-// Execute SQL statements from strings.
+// 객체의 키와 타입을 생성한다.
 db.exec(`
   CREATE TABLE data(
-    key INTEGER PRIMARY KEY,
-    value TEXT
+    name TEXT PRIMARY KEY,
+    value TEXT,
+    bio TEXT
   ) STRICT
-`);
-// Create a prepared statement to insert data into the database.
-const insert = db.prepare('INSERT INTO data (key, value) VALUES (?, ?)');
-// Execute the prepared statement with bound values.
-insert.run(1, 'hello');
-insert.run(2, 'world');
-// Create a prepared statement to read data from the database.
-const query = db.prepare('SELECT * FROM data ORDER BY key');
+`)
+// 자리에 맞춰 키와 값을 넣을 수 있다.
+// name : 첫번째 ?, value : 두번째 ?, bio : 세번째 ?
+const insert = db.prepare('INSERT INTO data (name, value, bio) VALUES (?, ?, ?)')
+
+// 순서에 맞게 값들을 삽입한다.
+insert.run('안녕', 'hello', '좋아')
+insert.run('반가워', 'world', "나도")
+// insert.run('안녕', 'world', "나도") 
+// -> PRIMARY KEY 부분에 같은 값이 들어가면 오류가 생긴다
+
+// 생성된 전체 데이터를 읽어온다.
+const query = db.prepare('SELECT * FROM data ORDER BY name')
 // Execute the prepared statement and log the result set.
-console.log(query.all());
-// Prints: [ { key: 1, value: 'hello' }, { key: 2, value: 'world' } ]
+console.log(query.all())
 
 // node --experimental-sqlite src/sql-lite/sqlite-database.js <- 실행 명령어
