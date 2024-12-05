138a61d07ebcd70e29d276fdb7c350767b3879e2
pukkok <krystalgggg@gmail.com>
2024-12-05 18:08:27 +0900

    sql로 데이터베이스 연결

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from 'http'
 
 import mainPage from './server-action/get-process.js'
 import postResponse from './server-action/post-process.js'
+import postSqlResponse from './server-action/post-sql-action.js'
 
 const PORT = 8080
 
@@ -14,7 +15,9 @@ const server = http.createServer((req, res) => {
 
   if(req.method === "POST") {
     if(req.url === '/send-data') {
-      postResponse(req, res)
+      // postResponse(req, res)
+
+      postSqlResponse(req, res)
     }
   }
 
