7dd02f9c04ad0e30035bd06563b0391ebba2128e
pukkok <krystalgggg@gmail.com>
2024-12-05 09:34:48 +0900

    공휴일 정보 api 불러오기 기능 추가

--- src/server.js
+++ src/server.js
@@ -1,6 +1,20 @@
 const http = require('http')
+const config = require('../config.js')
 
-
+// getRestDeInfo
+// getAnniversaryInfo
+const getRestInfo = async (year=2025, month=10) => {
+  const query = `&solYear=${year}&numOfRows=30`
+  const url = `${config.BASE_URL}/getRestDeInfo?_type=json${query}&ServiceKey=${config.SECRET_KEY}`
+  const result =  await fetch(url, {
+      method : 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+  )
+  return await result.json()
+}
 
 const server = http.createServer((req, res) => {
 
@@ -10,9 +24,16 @@ const server = http.createServer((req, res) => {
     res.end('안녕하세요')
   }
 
-  
+  if(req.url === '/rest-info') {
+    new Promise((resolve, reject) => {
+      resolve(getRestInfo())
+    }).then(data => {
+      res.writeHead(200, {"Content-Type": "application/json"})
+      res.end(JSON.stringify(data))
+    })
+  }
 })
 
 server.listen(8080, () => {
-  console.log('서버 동작: http://localhost:8080/test')
+  console.log('서버 동작: http://localhost:8080/rest-info')
 })
