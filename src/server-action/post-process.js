13301b00db150f2daed8525547c9d24250ec97ad
pukkok <krystalgggg@gmail.com>
2024-12-05 12:26:20 +0900

    데이터 저장기능 추가

--- src/server-action/post-process.js
+++ src/server-action/post-process.js
@@ -1,4 +1,6 @@
 import mainPage from "./get-process.js"
+import fs from 'fs'
+
 
 const postResponse = (req, res, body) => {
   req.on("data", (chunk) => {
@@ -7,6 +9,14 @@ const postResponse = (req, res, body) => {
 
   req.on("end", () => {
     console.log(body)
+    if(body !== "") {
+      fs.writeFile("./storage/minji.txt", body, (err) => {
+        if(err) return console.err(err)
+        
+        console.log('파일이 저장되었습니다.')
+      })
+    }
+
     mainPage(res)
   })
 }
