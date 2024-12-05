0b32682de736d7a9cf1294cb07c0c826d7d34441
pukkok <krystalgggg@gmail.com>
2024-12-05 10:59:09 +0900

    index.html 불러오기 작업 설정 완료

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import http from 'http'
+import fs from 'fs'
+import path from 'path'
 const PORT = 8080
 
 /**
  * 
  * ? 두개의 조건
- * GET
- * POST
+ * * GET
+ * * POST
  * 
  * 이외에도 PUT, DELETE, PATCH 등 이 있다.
  */
@@ -13,10 +15,22 @@ const PORT = 8080
 const server = http.createServer((req, res) => {
   if(req.method === "GET") {
     // TODO : GET은 주소 접속할 때만 쓸꺼야.
+
+    // ? index.html 로부터 입력 두개를 준비해야돼
+
     if(req.url === "/") {
       // TODO : "/" index.html 페이지를 이야기한다.
       // TODO : index.html 관련한 파일들을 응답해 주기로 한다.
 
+      // * fs 패키지가 필요할 것 같은데?
+      // * path 패키지도 필요할 것 같은데?
+
+      const __dirname = path.resolve()
+
+      const getHTML = fs.readFileSync(path.join(__dirname, "/public/index.html"), "utf-8")
+      res.writeHead(200, {"content-type" : "text/html"})
+      res.write(getHTML)
+      res.end()
 
     }
   }
@@ -29,7 +43,7 @@ const server = http.createServer((req, res) => {
 
     if(req.url === "/minseok") {
       // TODO : index.html 에서 <input> 사용
-      
+
     }
 
   }
