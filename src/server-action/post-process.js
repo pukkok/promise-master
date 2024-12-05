76e0fbfb458283337f5bb01daf30aaadc39264b1
pukkok <krystalgggg@gmail.com>
2024-12-05 16:41:01 +0900

    여러 데이터를 객체로 받도록 수정

--- src/server-action/post-process.js
+++ src/server-action/post-process.js
@@ -3,26 +3,31 @@ import loadDB from "../database/load-database.js"
 import saveDataBase from "../database/save-database.js"
 
 const postResponse = (req, res) => {
-  let body = ""
+  let responseData = {}
 
   req.on("data", (chunk) => {
-    body += decodeURIComponent(String(chunk).replace('+', ' ').trim())
+    const params = String(chunk).split("&")
+    params.forEach(param => {
+      const [key, value] = param.split('=')
+      if(value !== "") {
+        responseData[key] = decodeURIComponent(value.replace(/\+/g, ' ')) // 띄어쓰기 처리를 디코드 전에 처리한다.
+      }
+    })
     
+    // console.log(responseData) // 결과 확인용
   })
 
   req.on("end", () => {
-    if(body !== "") {
-      const [key, value] = body.split('=')
 
       loadDB()
       .then(parseData => {
         const updateData = {
-          ...parseData,
-          [key] : value
+          ...parseData, // 원래 들어있던 데이터
+          ...responseData // 응답 들어온 데이터로 업데이트
         }
         saveDataBase(updateData)
       })
-    }
+    
 
     mainPage(res)
   })
